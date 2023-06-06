63db2195e2245a638cf80c30ebd367e75b765986
Abdi <inventorabdi@gmail.com>
2023-06-06 06:47:51 -0700

    car factory created

--- engine/car/car.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-class Car(ABC):
-    def __int__(self, Engine, Battery):
-        self.Engine = Engine
-        self.Battery = Battery
-    @abstractmethod
-    def needs_service(self):
-        pass
-        #return self.Battery.needs_service or self.Engine.needs_service()
--- engine/sternman_engine.py
+++ engine/sternman_engine.py
@@ -1,15 +1,8 @@
-from abc import ABC
 
-from car import Car
+from . engine import Engine
 
-
-class SternmanEngine(Car, ABC):
-    def __init__(self, last_service_date, warning_light_is_on):
-        super().__init__(last_service_date)
-        self.warning_light_is_on = warning_light_is_on
-
-    def engine_should_be_serviced(self):
-        if self.warning_light_is_on:
-            return True
-        else:
-            return False
+class SternManEngine(Engine):
+    def __init__(self, warning_light_is_on):
+        self.warning_light_is_on = self.warning_light_is_on
+    def needs_service(self):
+        return self.warning_light_is_on
